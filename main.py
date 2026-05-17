d6399faa9281a6caca2816f5c161ca4b082efa30
ana carnina <lalesafarzade@gmail.com>
2026-05-17 15:39:29 -0400

    adding py file

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from stats import run_ztest
+
+app = FastAPI()
+
+# Allow frontend to call API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
+
+@app.post("/analyze")
+async def analyze(file: UploadFile):
+    df = pd.read_csv(file.file)
+
+    summary, z_stat, p_value = run_ztest(df)
+    summary["conversion_rate"] = summary["sum"] / summary["count"]
+    result={
+        "z_stat":z_stat,
+        "p_value":p_value,
+        "conversion_rate":summary["conversion_rate"]
+    }
+
+    return result
---
+++ stats.py
@@ -0,0 +1,20 @@
+import numpy as np
+from statsmodels.stats.proportion import proportions_ztest
+import scipy.stats as stats
+
+def run_ztest(df):
+    summary = df.groupby("group")["retention_1"].agg(["sum", "count"])
+
+    conversions = summary["sum"].values
+    nobs = summary["count"].values
+
+    z_stat, p_value = proportions_ztest(conversions, nobs)
+
+    return summary, z_stat, p_value
+
+
+def confidence_interval(p, n, alpha=0.05):
+    z = stats.norm.ppf(1 - alpha/2)
+    se = np.sqrt(p * (1 - p) / n)
+
+    return (p - z * se, p + z * se)
