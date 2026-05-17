969c39c2f5a8772bf93052866a69912511bfbddf
ana carnina <lalesafarzade@gmail.com>
2026-05-17 17:02:49 -0400

    adding result

--- static/assets/script.js
+++ static/assets/script.js
@@ -37,6 +37,14 @@ async function upload() {
     console.log(data);}
 
 
+document.getElementById("result").innerHTML = `
+    <h3>A/B Test Results</h3>
+    <p>Conversion A: ${data.conversion_rate.A}</p>
+    <p>Conversion B: ${data.conversion_rate.B}</p>
+    <p>P-Value: ${data.p_value}</p>
+    <p>Z-Statistic: ${data.z_stat}</p>
+`;
+
 
     // Take first numeric column
 
